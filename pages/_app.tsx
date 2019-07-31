b643916bdbc8d16b5431bb75f17a41ff5e4ae3a4
Ben Pearey <bpearey@amplience.com>
2019-07-31 17:08:01 +0100

    feat(seo): adding default seo information to the head

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import App, { Container } from 'next/app';
+import React from 'react';
+import { NextComponentType, NextPageContext } from 'next';
+import { DefaultSeo } from 'next-seo';
+
+import SEO from '../next-seo.config';
+
+interface InitialPropsConfig {
+  Component: NextComponentType;
+  ctx: NextPageContext;
+}
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }: InitialPropsConfig) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
