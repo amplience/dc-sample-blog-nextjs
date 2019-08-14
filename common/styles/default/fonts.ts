3d7746e6ea34fd4e049c6880acbd49c1cbe23cf4
Ben Pearey <bpearey@amplience.com>
2019-08-14 17:07:39 +0100

    refactor(hero banner): using component font styles over global

--- common/styles/default/fonts.ts
+++ common/styles/default/fonts.ts
@@ -1,17 +1,7 @@
 const fonts = {
   size: {
     root: '14px',
-    mobileRoot: '18px',
-    tiny: '0.6rem',
-    small: '0.8rem',
-    normal: '1rem',
-    large: '1.2rem',
-    xLarge: '2rem'
-  },
-  weight: {
-    light: '300',
-    normal: 'normal',
-    bold: '700'
+    mobileRoot: '18px'
   }
 };
 
