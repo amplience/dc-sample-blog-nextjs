ad70991ac8de72819015be5c49697e93566ea7e7
Simon Mudd <smudd@amplience.com>
2019-08-22 17:32:59 +0100

    fix(displayed date): fixing the date displayed

--- common/services/blog-date.service.spec.ts
+++ common/services/blog-date.service.spec.ts
@@ -5,10 +5,6 @@ describe('blog-date.service', (): void => {
     expect(convertToBlogDate('2019-08-14')).toEqual('14 August, 2019');
   });
 
-  test('convertToBlogDate should format a date string using a custom format', (): void => {
-    expect(convertToBlogDate('2019-08-14', 'YYYY')).toEqual('2019');
-  });
-
   test('convertToBlogDate should return an empty string when no date value passed', (): void => {
     expect(convertToBlogDate(undefined)).toEqual('');
   });
