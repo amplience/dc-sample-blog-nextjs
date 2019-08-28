f68ef8662cd1d206b28019d68115c04d4cf26f67
Simon Mudd <smudd@amplience.com>
2019-08-28 13:48:22 +0100

    fix(image src): adding url component encoding for image names when making a src

--- common/services/media.service.ts
+++ common/services/media.service.ts
@@ -1,5 +1,5 @@
 import Media from '../interfaces/media.interface';
 
 export default function buildMediaUrl(media: Media): string {
-  return `//${media.defaultHost}/${media.mediaType}/${media.endpoint}/${media.name}`;
+  return `//${media.defaultHost}/${media.mediaType}/${media.endpoint}/${encodeURIComponent(media.name)}`;
 }
