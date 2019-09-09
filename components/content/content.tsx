563046ed7020c28965bf46c8cf96123698137289
Ben Pearey <bpearey@amplience.com>
2019-09-09 12:51:56 +0100

    fix(blog paragraph): make sure we dont apply a margin for first instance of content paragraph

--- components/content/content.tsx
+++ components/content/content.tsx
@@ -47,7 +47,7 @@ const Content = ({ content }: { content: AmplienceContent[] }) => {
         }
 
         section > div {
-          margin-bottom: 75px;
+          margin-top: 15px;
           width: 100%;
         }
 
@@ -69,9 +69,13 @@ const Content = ({ content }: { content: AmplienceContent[] }) => {
           background-color: ${theme.colors.whiteLilac};
         }
 
+        section :global(p:first-child) {
+          margin-top: 0;
+        }
+
         @media (max-width: ${theme.layout.narrowPageWidth}) {
-          section > div {
-            margin-top: 90px;
+          section > div:first-child {
+            margin-top: 15px;
           }
         }
       `}</style>
