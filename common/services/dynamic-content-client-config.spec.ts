abed31a587e170c83dd9a9dc339135029985f248
Ben Pearey <bpearey@amplience.com>
2019-10-07 16:57:55 +0100

    docs(readme): documenting secure media host env var
    - removed media host env var as we don't set the protocol and always default to secure

--- common/services/dynamic-content-client-config.spec.ts
+++ common/services/dynamic-content-client-config.spec.ts
@@ -5,7 +5,6 @@ describe('dynamic-content-client-config', (): void => {
     jest.resetModules();
     process.env = { ...OLD_ENV };
     delete process.env.DYNAMIC_CONTENT_ACCOUNT_NAME;
-    delete process.env.DYNAMIC_CONTENT_MEDIA_HOST;
     delete process.env.DYNAMIC_CONTENT_SECURE_MEDIA_HOST;
   });
 
@@ -14,14 +13,12 @@ describe('dynamic-content-client-config', (): void => {
   });
   test('it should set all config values to their respective env var', (): void => {
     process.env.DYNAMIC_CONTENT_ACCOUNT_NAME = 'account-id';
-    process.env.DYNAMIC_CONTENT_MEDIA_HOST = 'custom-media-host.amplience.net';
     process.env.DYNAMIC_CONTENT_SECURE_MEDIA_HOST = 'custom-secure-media-host.amplience.net';
 
     const defaultClientConfig = require('./dynamic-content-client-config').defaultClientConfig;
 
     expect(defaultClientConfig).toEqual({
       account: 'account-id',
-      mediaHost: 'custom-media-host.amplience.net',
       secureMediaHost: 'custom-secure-media-host.amplience.net'
     });
   });
@@ -31,7 +28,6 @@ describe('dynamic-content-client-config', (): void => {
 
     expect(defaultClientConfig).toEqual({
       account: '',
-      mediaHost: '',
       secureMediaHost: ''
     });
   });
--- common/services/dynamic-content-client-config.ts
+++ common/services/dynamic-content-client-config.ts
@@ -2,6 +2,5 @@ import { ContentClientConfig } from 'dc-delivery-sdk-js';
 
 export const defaultClientConfig: ContentClientConfig = {
   account: process.env.DYNAMIC_CONTENT_ACCOUNT_NAME || '',
-  mediaHost: process.env.DYNAMIC_CONTENT_MEDIA_HOST || '',
   secureMediaHost: process.env.DYNAMIC_CONTENT_SECURE_MEDIA_HOST || ''
 };
