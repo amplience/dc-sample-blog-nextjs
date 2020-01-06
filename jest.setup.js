2b4699e7eb294a6b59f5ea367ff0b7dddf8e9443
Marc Easen <measen@amplience.com>
2020-01-06 12:00:33 +0000

    test(date): pinned the date to be 2019-01-02T03:04:05.000Z

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,13 @@ const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// Pin the date, to avoid the tests from breaking when the year change
+global.Date = class extends Date {
+  constructor(...args) {
+    super(...args);
+    if (args.length === 0) {
+      return new Date('2019-01-02T03:04:05.000Z');
+    }
+  }
+};
