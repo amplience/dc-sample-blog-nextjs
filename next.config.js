84a9b6b4b2adb9cf6dc01668c4dfbc442f672918
Ben Pearey <bpearey@amplience.com>
2020-06-16 11:40:37 +0100

    refactor(next config): removing deprecated static folder in favor of a public folder
    see: https://github.com/vercel/next.js/blob/master/errors/static-dir-deprecated.md

--- next.config.js
+++ next.config.js
@@ -1,27 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/camelcase*/
-const fs = require('fs');
 const flow = require('lodash.flow');
 const withManifest = require('next-manifest');
 const withOffline = require('next-offline');
 const ContentClient = require('dc-delivery-sdk-js').ContentClient;
 const allSettled = require('promise.allsettled');
 require('dotenv').config();
-
-const copyFilesRecursively = (sourceDir, destDir) => {
-  if (!fs.existsSync(destDir)) {
-    fs.mkdirSync(destDir);
-  }
-
-  const files = fs.readdirSync(sourceDir);
-  files.forEach(file => {
-    if (fs.lstatSync(`${sourceDir}/${file}`).isDirectory()) {
-      copyFilesRecursively(`${sourceDir}/${file}`, `${destDir}/${file}`);
-    } else {
-      fs.copyFileSync(`${sourceDir}/${file}`, `${destDir}/${file}`);
-    }
-  });
-};
 
 const checkForDuplicateSlugs = blogPosts => {
   let seen = new Set();
@@ -88,16 +71,6 @@ const getBlogList = async () => {
 const exportPathMap = async function () {
   let dynamicPages = {};
 
-  console.info('Copying public folder to out');
-  const outDir = `${__dirname}/out`;
-  const publicDir = `${__dirname}/static/public`;
-  try {
-    copyFilesRecursively(publicDir, outDir);
-  } catch (err) {
-    console.error('Error copying public files to out dir');
-    throw err;
-  }
-
   try {
     const blogList = await getBlogList();
     checkForDuplicateSlugs(blogList.blogPosts);
@@ -153,7 +126,7 @@ const manifest = {
   Scope: '/',
   start_url: '/',
   cache: true,
-  output: './static/',
+  output: './public/static/',
   icons: [
     {
       src: '/static/icons/icon-72x72.png',
