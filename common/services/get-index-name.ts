1f2ddf5dcf6c447cb9e2721f11c0fe0fa5546ea1
Ben Pearey <bpearey@amplience.com>
2020-07-02 12:39:43 +0100

    fix(get index name): resolving type checking error

--- common/services/get-index-name.ts
+++ common/services/get-index-name.ts
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 
 const getIndexName = (): string => {
   const router = useRouter();
-  if (router?.query?.vse?.length > 0) {
+  if (router.query?.vse) {
     return process.env.ALGOLIA_STAGING_INDEX_NAME as string;
   }
   return process.env.ALGOLIA_PRODUCTION_INDEX_NAME as string;
