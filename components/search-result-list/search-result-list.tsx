63f3aaa03113651b3231e67f2fc4c48ff644f2fd
Marc Easen - Amplience <50992156+easen-amp@users.noreply.github.com>
2020-07-06 11:50:44 +0100

    fix(search-list): filters out any non blog-post items (#35)
    Co-authored-by: paulgturner <61312164+paulgturner@users.noreply.github.com>

--- components/search-result-list/search-result-list.tsx
+++ components/search-result-list/search-result-list.tsx
@@ -1,41 +1,29 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { connectStateResults } from 'react-instantsearch-dom';
 import BlogPost from '../../common/interfaces/blog-post.interface';
 import NoBlogPosts from '../blog-list/no-blog-posts';
 import LoadingBlogPosts from '../blog-list/loading-blog-posts';
 import HeroCard from '../hero-card/hero-card';
 import BlogList from '../blog-list/blog-list';
-
-import { SearchResults, BasicDoc } from 'react-instantsearch-core';
+import { StateResultsProvided } from 'react-instantsearch-core';
 import NoResults from '../blog-list/no-results';
 import SortByDropdown from './sort-by-dropdown';
+import { isBlogPost } from '../../common/services/blog-post.service';
 
-const flattenBlogPosts = (searchResults: SearchResults<BasicDoc>): BlogPost[] => {
-  if (!searchResults || searchResults.nbHits === 0) {
-    return [];
-  }
-
-  const blogPosts: BlogPost[] = [];
-  searchResults.hits.forEach((hit: BasicDoc) => {
-    blogPosts.push((hit as unknown) as BlogPost);
-  });
-  return blogPosts;
-};
-
-const SearchResultList = connectStateResults(({ searchResults }) => {
+const BlogPostSearchResultList: FunctionComponent<StateResultsProvided<BlogPost>> = ({ searchResults }) => {
   if (!searchResults) {
     return <LoadingBlogPosts />;
   }
 
-  if (searchResults.query.length > 0 && searchResults.nbHits === 0) {
+  if (searchResults.query.length > 0 && searchResults.hits.length === 0) {
     return <NoResults query={searchResults.query} />;
   }
 
-  if (searchResults.nbHits === 0) {
+  if (searchResults.hits.length === 0) {
     return <NoBlogPosts />;
   }
 
-  const blogPosts = flattenBlogPosts(searchResults);
+  const blogPosts = searchResults.hits.filter(isBlogPost);
   return (
     <div id="searchResults">
       <SortByDropdown />
@@ -43,6 +31,7 @@ const SearchResultList = connectStateResults(({ searchResults }) => {
       <BlogList blogPosts={blogPosts.slice(1)} />
     </div>
   );
-});
+};
 
+const SearchResultList = connectStateResults(BlogPostSearchResultList);
 export default SearchResultList;
